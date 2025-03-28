1b3cf4e1e81c795b00310786d4a3edc93c4bdd58
kimgleam <valva7@naver.com>
2025-03-28 16:42:43 +0900

    refactor: entity명 변경

--- src/main/java/org/musicshare/domain/like/model/entity/LikeEntity.java
+++ src/main/java/org/musicshare/domain/like/model/entity/LikeEntity.java
@@ -1,25 +1,14 @@
 package org.musicshare.domain.like.model.entity;
 
-import jakarta.persistence.Column;
 import jakarta.persistence.EmbeddedId;
 import jakarta.persistence.Entity;
-import jakarta.persistence.FetchType;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
-import jakarta.persistence.JoinColumn;
-import jakarta.persistence.ManyToOne;
 import jakarta.persistence.Table;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Getter;
 import lombok.NoArgsConstructor;
 import lombok.Setter;
-import org.musicshare.domain.like.LikeType;
 import org.musicshare.domain.like.model.Like;
-import org.musicshare.domain.member.model.Member;
-import org.musicshare.domain.music.model.Comment;
-import org.musicshare.domain.music.model.Music;
 import org.musicshare.global.entity.TimeBaseEntity;
 
 @Entity
@@ -28,7 +17,7 @@ import org.musicshare.global.entity.TimeBaseEntity;
 @NoArgsConstructor
 @AllArgsConstructor
 @Builder
-@Table(name = "like_entity")
+@Table(name = "likes")
 public class LikeEntity extends TimeBaseEntity {
     @EmbeddedId
     private LikeIdEntity id;
